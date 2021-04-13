92befa3bd2ee34ec4745475d52ade5eaa4133877
BurakPozut <burakpozut88@gmail.com>
2021-04-13 19:30:18 +0300

    Create MergeSort.py

---
+++ Search and Sort/MergeSort.py
@@ -0,0 +1,34 @@
+def merge_sort(array):
+    if len(array) > 1:
+        mid = len(array)//2
+        left_half = array[:mid]
+        right_half = array[mid:]
+
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        i = 0
+        j = 0
+        k = 0
+        while i < len(left_half) and j < len(right_half):
+            if left_half[i] < right_half[i]:
+                array[k] = left_half[i]
+                i += 1
+            else:
+                array[k] = right_half[j]
+                j += 1
+            k += 1
+
+        while i < len(left_half):
+            array[k] = left_half[i]
+            i += 1
+            k += 1
+        while j < len(right_half):
+            array[k] = right_half[j]
+            j += 1
+            k += 1
+    print(array)    # This print will show all the steps this algo. did
+
+
+array = [1, 55, 67, 12, 2, 45, 85, 6]
+merge_sort(array)
